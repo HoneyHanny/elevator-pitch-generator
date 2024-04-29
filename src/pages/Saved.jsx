64b2fadd587e37a52fb9728a0a2ceb04937516df
HoneyHanny <hansduran25@gmail.com>
2024-04-29 16:38:51 +0800

    Add saved page

--- src/pages/Saved.jsx
+++ src/pages/Saved.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
 import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 import useTheme from '@mui/material/styles/useTheme';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import FormGroup from '@mui/material/FormGroup';
 
 const Saved = () => {
   const theme = useTheme();
+
+  const buttons = [];
+  for (let i = 0; i < 5; i++) {
+    buttons.push(
+      <Button
+        variant='contained'
+        sx={{
+          borderRadius: '32px',
+        }}
+      >
+        Show
+      </Button>
+    );
+  }
+
   return (
     <Box
       sx={{
@@ -15,6 +34,7 @@ const Saved = () => {
       }}
     >
       <h1 style={{ color: theme.palette.primary.main }}>Saved List</h1>
+      {/* TODO(hans): Add each venn diagram to it's own component */}
       <Box
         sx={{
           padding: '1rem',
@@ -23,6 +43,97 @@ const Saved = () => {
         <h1 style={{ color: theme.palette.primary.main }}>
           2 Venn diagram list
         </h1>
+      </Box>
+      <Box
+        sx={{
+          padding: '64px',
+          borderRadius: '64px',
+          backgroundColor: 'gray.main',
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+        }}
+      >
+        {/* Left (checkboxes) */}
+        <Box>
+          <FormGroup>
+            {/* NOTE(hans): Use array.map later */}
+            <FormControlLabel
+              control={<Checkbox defaultChecked />}
+              label='Inefficient waste collection'
+            />
+            <FormControlLabel
+              control={<Checkbox />}
+              label='Limited availability of recycled products'
+            />
+            <FormControlLabel
+              control={<Checkbox />}
+              label='Limited access to recycling'
+            />
+            <FormControlLabel
+              control={<Checkbox />}
+              label='Accumulation of waste'
+            />
+            <FormControlLabel
+              control={<Checkbox />}
+              label='Resource depletion'
+            />
+          </FormGroup>
+        </Box>
+        {/* Right (buttons) */}
+        <Box flexDirection={'column'} display={'flex'} gap={'10px'}>
+          {buttons}
+        </Box>
+      </Box>
+      <Box
+        sx={{
+          padding: '1rem',
+        }}
+      >
+        <h1 style={{ color: theme.palette.primary.main }}>
+          3 Venn diagram list
+        </h1>
+      </Box>
+      <Box
+        sx={{
+          padding: '64px',
+          borderRadius: '64px',
+          backgroundColor: 'gray.main',
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+        }}
+      >
+        {/* Left (checkboxes) */}
+        <Box>
+          <FormGroup>
+            {/* NOTE(hans): Use array.map later */}
+            <FormControlLabel
+              control={<Checkbox defaultChecked />}
+              label='Inefficient waste collection'
+            />
+            <FormControlLabel
+              control={<Checkbox />}
+              label='Limited availability of recycled products'
+            />
+            <FormControlLabel
+              control={<Checkbox />}
+              label='Limited access to recycling'
+            />
+            <FormControlLabel
+              control={<Checkbox />}
+              label='Accumulation of waste'
+            />
+            <FormControlLabel
+              control={<Checkbox />}
+              label='Resource depletion'
+            />
+          </FormGroup>
+        </Box>
+        {/* Right (buttons) */}
+        <Box flexDirection={'column'} display={'flex'} gap={'10px'}>
+          {buttons}
+        </Box>
       </Box>
     </Box>
   );
